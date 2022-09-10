7afaf1d62b4b556c57645bf97b40cc463f11a63e
Hassaan Mahboob <hassaan.mahboob@tintash.com>
2022-09-10 22:13:15 +0500

    WHAT:Add testcases and refactoring
    WHY:to follow guidelines and convention
    
    IMPACT:LOW

--- notesapplication/notes/models.py
+++ notesapplication/notes/models.py
@@ -15,7 +15,7 @@ class Note(models.Model):
     text = models.CharField(max_length=199)
     date_created = models.DateField(default=str(date.today()))
     date_updated = models.DateField(default="")
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, default="")
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     archive = models.BooleanField(default=False)
     sharedwith = models.ManyToManyField(Users, related_name="sharedwith", blank=True)
 
@@ -28,8 +28,8 @@ class NoteVersion(models.Model):
     NoteVersion class to add versions of a note
     """
 
-    note_id = models.ForeignKey(Note, on_delete=models.CASCADE, default="")
-    edited_by = models.ForeignKey(Users, on_delete=models.CASCADE, default="")
+    note_id = models.ForeignKey(Note, on_delete=models.CASCADE)
+    edited_by = models.ForeignKey(Users, on_delete=models.CASCADE)
     title = models.CharField(max_length=39, default="")
     text = models.CharField(max_length=299)
     date_created = models.DateField(default=str(date.today()))
@@ -41,9 +41,7 @@ class Comment(models.Model):
     """
 
     text = models.CharField(max_length=299, default="")
-    note_id = models.ForeignKey(
-        Note, on_delete=models.CASCADE, default="", related_name="comment"
-    )
+    note_id = models.ForeignKey(Note, on_delete=models.CASCADE, related_name="comment")
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     date_created = models.DateField(default=str(date.today()))
     date_updated = models.DateField(default=str(date.today()))
