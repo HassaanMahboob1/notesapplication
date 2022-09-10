cd7bc1093ff7e0b250d4e3438798fbd01afe500e
Hassaan Mahboob <hassaan.mahboob@tintash.com>
2022-09-10 22:09:26 +0500

    WHAT:Add testcases and refactoring
    WHY:to follow guidelines and convention
    
    IMPACT:LOW

--- notesapplication/notes/api_v1/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_delete
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from notes.models import Note
-
-
-@receiver(post_delete, sender=Note, dispatch_uid="1122")
-def create_note(sender, instance, created, **kwargs):
-    print("Note created")
--- notesapplication/user/api_v1/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from notes.models import Users
-
-
-@receiver(post_save, sender=Users, dispatch_uid="1122")
-def create_profile(sender, instance, created, **kwargs):
-    print("User Created")
